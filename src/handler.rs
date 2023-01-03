1067f95c6b916dfdbb993c914a28b2a02fe6662e
Anton Whalley <anton@venshare.com>
2023-01-03 16:38:59 +0000

    feature: reinstate probes
    Signed-off-by: Anton Whalley <anton@venshare.com>

--- src/handler.rs
+++ src/handler.rs
@@ -2,9 +2,10 @@ use hyper::{Body, Method, Request, Response, StatusCode};
 pub async fn handle_request(req: Request<Body>) -> Result<Response<Body>, anyhow::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new(Body::from("Hi There!"))),
-
         (&Method::POST, "/") => Ok(Response::new(req.into_body())),
 
+        (&Method::GET, "/health/readiness") => Ok(Response::new(Body::from(""))),
+        (&Method::GET, "/health/liveness") => Ok(Response::new(Body::from(""))),
         _ => {
             let mut not_found = Response::default();
             *not_found.status_mut() = StatusCode::NOT_FOUND;
